39abbef00fe53b995320eb4f9c5ce6a0f6b5b903
nas11ai <74126181+nas11ai@users.noreply.github.com>
2022-04-28 20:25:54 +0800

    4.4: helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,11 @@
 const dummy = () => 1;
 
+const totalLikes = (blogs) => {
+  const likesReducer = (sum, obj) => sum + obj.likes;
+  return blogs.reduce(likesReducer, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
