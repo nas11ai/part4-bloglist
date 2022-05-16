67eb439bd2b2a11139e6d211dbf6006ed07742ac
nas11ai <74126181+nas11ai@users.noreply.github.com>
2022-05-16 17:01:57 +0800

    4.19: bloglist expansion, step7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
 const User = require('../models/user');
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -18,9 +27,13 @@ blogRouter.get('/:id', async (request, response) => {
 });
 
 blogRouter.post('/', async (request, response) => {
-  const { body } = request;
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!decodedToken) {
+    return response.status(401).json({ error: 'token is missing or invalid' });
+  }
 
-  const user = await User.findById(body.user);
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog(request.body);
 
@@ -30,7 +43,7 @@ blogRouter.post('/', async (request, response) => {
   user.blogs = user.blogs.concat(_id);
   await user.save();
 
-  response.status(201).json(savedBlog);
+  return response.status(201).json(savedBlog);
 });
 
 blogRouter.delete('/:id', async (request, response) => {
--- utils/middleware.js
+++ utils/middleware.js
@@ -9,9 +9,16 @@ const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
-  } if (error.name === 'ValidationError') {
+  }
+
+  if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
   }
+
+  if (error.name === 'JsonWebTokenError') {
+    return response.status(400).json({ error: 'invalid token' });
+  }
+
   return next(error);
 };
 
